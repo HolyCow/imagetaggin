a076d919644ffb36bae2bf51b5b29e1d79c63753
vagrant <vagrant@precise32.(none)>
2013-06-11 16:54:57 +0000

    add application

---
+++ imagetaggin_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImagetagginApp::Application.config.secret_token = 'e932a852b5c40a4629dc0eb63dd4a938343c37f708b07425df9cc5f7dd9e8f20699115ef05818919185765d01b2f8ec55f7eca4b92457dccdfb656dda864c317'
---
+++ imagetaggin_app/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ImagetagginApp::Application.config.session_store :cookie_store, key: '_imagetaggin_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ImagetagginApp::Application.config.session_store :active_record_store
