e74db8434359696addba4e22c88550caf77331f4
HolyCow <mike.beasterfeld@gmail.com>
2013-06-11 11:53:07 -0500

    remove application directory

--- imagetaggin/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Imagetaggin::Application.config.secret_token = 'a73f7e61e74c471a3fb24940ecf0e710156227b5b98fc974ee33d2d3aabbcf3a66bebbef4a6f5a3c8387d0fc8df6524e8be3d8f8d9d3fe4800b3351a2c16ae96'
